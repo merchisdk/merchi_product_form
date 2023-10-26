ccbc4fd380a6c3f6a2c247812d11677b9d06c7eb
s3341458 <you@example.com>
2023-10-26 12:12:50 +1100

    merchi product form published and worked at first time

--- src/actions/jobs.ts
+++ src/actions/jobs.ts
@@ -1,12 +1,13 @@
 import { encodeMerchiApiData } from './helpers';
 import { urlSearchParams } from '../utils/url';
 
-export async function fetchJobQuote(jobJson: any) {
+export async function fetchJobQuote(jobJson: any, apiUrl: string) {
+
   const formData = encodeMerchiApiData(jobJson);
   const queryString = urlSearchParams({product_id:  jobJson.product!.id ? String(jobJson.product!.id!) : 'null', skip_rights: 'y'});
   const fetchOptions: any = {method: 'POST', body: formData};
   const response = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}specialised-order-estimate/?${queryString}`,
+    `${apiUrl}/specialised-order-estimate/?${queryString}`,
     fetchOptions,
   );
   if (!response.ok) {
