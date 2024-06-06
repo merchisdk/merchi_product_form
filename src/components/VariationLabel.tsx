a4123b63fc6f54974f465adb11dad44d9c1a0980
James Cool <jamescool@Jamess-MacBook-Pro.local>
2024-06-06 19:07:40 +1000

    bump version

--- src/components/VariationLabel.tsx
+++ src/components/VariationLabel.tsx
@@ -3,6 +3,7 @@ import { variationCostDetail } from './utils';
 import VariationFieldNameAndCostInputs from './VariationFieldNameAndCostInputs';
 import { useMerchiFormContext } from './MerchiProductFormProvider';
 import { CgSpinner } from 'react-icons/cg';
+import VariationFieldInputInstructions from './VariationFieldInputInstructions';
 
 interface Props {
   forceHideCost?: boolean;
@@ -18,33 +19,36 @@ function VariationLabel({
   variationClassName,
 }: Props) {
   const { cost, variationField, onceOffCost, unitCost } = variation;
-  const { sellerProductEditable } = variationField;
+  const { instructions, sellerProductEditable } = variationField;
   const { hideCost, loading } = useMerchiFormContext();
   const hasExtraCost = onceOffCost || unitCost;
   return (
-    <div
-      className={`d-flex align-items-center mb-1 ${variationClassName || ''}`}
-    >
+    <>
       <div
-        className={`align-items-center flex-wrap gap-1${
-          variationClassName ? variationClassName + '-title' : ''
-        }`}
+        className={`d-flex align-items-center mb-1 ${variationClassName || ''}`}
       >
-        {`${variationField!.name} `}
-        {loading && cost ? (
-          <CgSpinner fontSize='1.25rem' className='animate_spin ml-1' />
-        ) : hideCost || forceHideCost ? (
-          ''
-        ) : hasExtraCost && !sellerProductEditable ? (
-          <span className='merchi-embed-form_variation-cost-detail'>
-            {variationCostDetail(variation)}
-          </span>
-        ) : (
-          ''
-        )}
+        <div
+          className={`align-items-center flex-wrap gap-1${
+            variationClassName ? variationClassName + '-title' : ''
+          }`}
+        >
+          {`${variationField!.name} `}
+          {loading && cost ? (
+            <CgSpinner fontSize='1.25rem' className='animate_spin ml-1' />
+          ) : hideCost || forceHideCost ? (
+            ''
+          ) : hasExtraCost && !sellerProductEditable ? (
+            <span className='merchi-embed-form_variation-cost-detail'>
+              {variationCostDetail(variation)}
+            </span>
+          ) : (
+            ''
+          )}
+        </div>
+        <VariationFieldNameAndCostInputs name={name} variation={variation} />
       </div>
-      <VariationFieldNameAndCostInputs name={name} variation={variation} />
-    </div>
+      {instructions && <VariationFieldInputInstructions instructions={instructions} />}
+    </>
   );
 }
 
