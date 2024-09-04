5d6e343af5f50298881548bf5937b6d1d5180c80
James Cool <jamescool@Jamess-MacBook-Pro.local>
2024-09-04 11:20:53 +1000

    add tags to jobs and cart items

--- src/components/utils.ts
+++ src/components/utils.ts
@@ -446,3 +446,23 @@ export function getCountryName(countryCode: any) {
     return countryCode;
   }
 }
+
+export function cleanTagIds(raw: any[]) {
+  return raw.map(id => id.trim())            // Trim each ID entry to remove whitespace
+    .filter(id => /^\d+$/.test(id))  // Use a regular expression to ensure the ID is entirely numeric.
+    .map(id => Number(id));          // Convert the remaining, valid ID entries into numbers
+}
+
+export function getMerchiSourceJobTags(): any[] {
+  if (typeof localStorage !== 'undefined' && localStorage !== null) {
+    const merchiSource = localStorage.getItem('merchi_source');
+
+    if (merchiSource) {
+      const ids = cleanTagIds(merchiSource.split(','));
+      return ids.map((id: number) => ({ id }));
+    }
+    // If "merchi_source" is not found in localStorage, return an empty array.
+    return [];
+  }
+  return [];
+}
