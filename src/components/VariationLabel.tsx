b9d4de2884908d85623bd3ee6068a6a96d6dd612
freyaLi321 <freyali0321@gmail.com>
2025-04-14 10:42:37 +1000

    align spinner with label and prevent 0 rendering

--- src/components/VariationLabel.tsx
+++ src/components/VariationLabel.tsx
@@ -22,29 +22,38 @@ function VariationLabel({
   const { instructions, sellerProductEditable } = variationField;
   const { hideCost, loading } = useMerchiFormContext();
   const hasExtraCost = onceOffCost || unitCost;
+
   return (
     <>
       <div
         className={`d-flex align-items-center mb-1 ${variationClassName || ''}`}
       >
         <div
-          className={`align-items-center flex-wrap gap-1${variationClassName ? variationClassName + '-title' : ''
-            }`}
+          style={{ display: 'inline-flex', alignItems: 'center', gap: '4px' }}
+          className={`align-items-center flex-wrap gap-1${
+            variationClassName ? variationClassName + '-title' : ''
+          }`}
         >
-          {`${variationField!.name} `}
-          {hideCost || forceHideCost ? (
+          {`${variationField?.name ?? ''} `}
+          {loading && cost ? (
+            <CgSpinner fontSize="1.25rem" className="animate_spin ml-1" />
+          ) : hideCost || forceHideCost ? (
             ''
           ) : hasExtraCost && !sellerProductEditable ? (
-            <span className='merchi-embed-form_variation-cost-detail'>
+            <span className="merchi-embed-form_variation-cost-detail">
               {variationCostDetail(variation)}
             </span>
           ) : (
             ''
           )}
         </div>
+
         <VariationFieldNameAndCostInputs name={name} variation={variation} />
       </div>
-      {instructions && <VariationFieldInputInstructions instructions={instructions} />}
+
+      {instructions && (
+        <VariationFieldInputInstructions instructions={instructions} />
+      )}
     </>
   );
 }
